1e2dc395d5f3c76677fbb2b4eed16719483b9ea6
Alrick <alrick.deillon@unifr.ch>
2016-02-24 11:00:16 +0100

    release

---
+++ platforms/android/CordovaLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/aebiscna/dev/phonegap/NTE/bibup/platforms/android/CordovaLib/bin/AndroidManifest.xml : \
+/Users/aebiscna/dev/phonegap/NTE/bibup/platforms/android/CordovaLib/AndroidManifest.xml \
